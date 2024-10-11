d0f9a9af427f9edf16e9feaafea9ffa45f3f730d
Santosh Areti <bhargavas.edu2022@gmail.com>
2024-10-11 11:14:08 -0500

    first commit

---
+++ src/test/java/dev/areti/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.areti.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
